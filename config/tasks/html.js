b44b142cd7ed5a323100731fabf86907880426d4
focusMinor <focus.minor@gmail.com>
2022-10-15 10:41:43 +0500

    changed GLI to GI

--- config/tasks/html.js
+++ config/tasks/html.js
@@ -1,10 +1,10 @@
-import fileInclude from "gulp-file-include";
+import gulpInclude from "gulp-include";
 import versionNumber from "gulp-version-number";
 
 export const html = () => {
    return app.gulp
       .src(app.path.src.html)
-      .pipe(fileInclude())
+      .pipe(gulpInclude())
       .pipe(app.plugins.replace(/@img\//g, "img/"))
       .pipe(
          app.plugins.ifPlugin(
